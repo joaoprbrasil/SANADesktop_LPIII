575d21d6fa4b30178a3fc706a22f8be0dc09a5c6
Edilson Marques <104401369+EdilsonMarquesCurzJr@users.noreply.github.com>
2024-07-30 19:57:05 -0300

    o

--- SANA/SANA/obj/Debug/net8.0-windows/SANA.AssemblyInfo.cs
+++ SANA/SANA/obj/Debug/net8.0-windows/SANA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SANA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5af6dffe226459987ca73006f25fc4b689858b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345116da3aae63d12b4239d305bb1583aa974291")]
 [assembly: System.Reflection.AssemblyProductAttribute("SANA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SANA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
